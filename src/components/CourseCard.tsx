b7b77ca9aa188a5085c7f52e6fda69ec9b424dd0
unknown <nguyenhuutan2502@gmail.com>
2026-05-16 09:02:36 +0700

    fix

--- src/components/CourseCard.tsx
+++ src/components/CourseCard.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
-import { useAppDispatch, useAppSelector } from '../Hooks/useRedux';
+import { useAppDispatch } from '../Hooks/useRedux';
 import { getDangKyKhoaHocThunk } from "../redux/reducer/DanhMucKhoaHocReducer";
 
 import type { Course } from '../Types';
